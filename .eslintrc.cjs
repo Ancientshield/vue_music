29a91d1887f1f7f18375cd807018a8dcb9bc3367
Ancientshield <jacklin2100@gmail.com>
2023-04-05 11:06:36 +0800

    disable eslint check Vue component multi-word

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,26 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
   ],
   overrides: [
     {
       files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'],
-      extends: ['plugin:cypress/recommended']
-    }
+      extends: ['plugin:cypress/recommended'],
+    },
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 'latest',
   },
   env: {
-    node: true
-  }
-}
+    node: true,
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off',
+  },
+};
