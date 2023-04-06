35052fb83e8e8e535f00e4183c698944e05baf58
Ancientshield <jacklin2100@gmail.com>
2023-04-06 20:46:37 +0800

    use mapWritableState at Auth.vue to change isOpen state in modal.js

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed z-10 inset-0 overflow-y-auto hidden" id="modal">
+  <div class="fixed z-10 inset-0 overflow-y-auto" id="modal" :class="hiddenClass">
     <div
       class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
     >
@@ -19,7 +19,7 @@
           <div class="flex justify-between items-center pb-4">
             <p class="text-2xl font-bold">Your Account</p>
             <!-- Modal Close Button -->
-            <div class="modal-close cursor-pointer z-50">
+            <div class="modal-close cursor-pointer z-50" @click="isOpen = false">
               <i class="fas fa-times"></i>
             </div>
           </div>
@@ -141,7 +141,14 @@
 </template>
 
 <script>
+import { mapState, mapWritableState } from 'pinia';
+import useModalStore from '@/stores/modal.js';
+
 export default {
   name: 'Auth',
+  computed: {
+    ...mapState(useModalStore, ['hiddenClass']),
+    ...mapWritableState(useModalStore, ['isOpen']),
+  },
 };
 </script>
