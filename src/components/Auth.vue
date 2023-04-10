d6abf3c59d15981c485dbef7e9b6bb3b0f6facc7
Ancientshield <jacklin2100@gmail.com>
2023-04-10 22:50:36 +0800

    Rendering Multiple Error Messages

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -117,13 +117,18 @@
             <!-- Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
-              <vee-field
-                type="password"
-                name="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password"
-              />
-              <ErrorMessage class="text-red-600" name="password" />
+              <vee-field name="password" :bails="false" v-slot="{ field, errors }">
+                <input
+                  type="password"
+                  class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+                  placeholder="Password"
+                  v-bind="field"
+                />
+                <div class="text-red-600" v-for="error in errors" :key="error">
+                  {{ error }}
+                </div>
+              </vee-field>
+              <!-- <ErrorMessage class="text-red-600" name="password" /> -->
             </div>
             <!-- Confirm Password -->
             <div class="mb-3">
@@ -189,7 +194,7 @@ export default {
         name: 'required|min:3|max:100|alpha_spaces',
         email: 'required|min:3|max:100|email',
         age: 'required|minVal:18|maxVal:100',
-        password: 'required|min:3|max:100',
+        password: 'required|min:9|max:100|excluded:password',
         confirm_password: 'required|confirmed:@password',
         country: 'required|excluded:Antarctica',
         tos: 'required',
