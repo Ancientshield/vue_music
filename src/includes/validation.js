1491a1d0f98b6652449cd91ee7be779c26d370f2
Ancientshield <ancientsword520@gmail.com>
2023-04-11 21:13:45 +0800

    add validation config situation

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,4 +1,9 @@
-import { Form as VeeForm, Field as VeeField, defineRule, ErrorMessage } from 'vee-validate';
+import { 
+  Form as VeeForm,
+  Field as VeeField,
+  defineRule, 
+  ErrorMessage,
+  configure } from 'vee-validate';
 import {
   required,
   min,
@@ -17,17 +22,41 @@ import {
 export default {
   install(app) {
     app.component('VeeForm', VeeForm);
-    app.component('VeeField', VeeField);
+    app.component('VeeField', VeeField); 
     app.component('ErrorMessage', ErrorMessage);
 
     defineRule('required', required);
+    defineRule('tos', required);
     defineRule('min', min);
     defineRule('max', max);
     defineRule('alphaSpaces', alphaSpaces);
     defineRule('email', email);
     defineRule('minVal', minVal);
     defineRule('maxVal', maxVal);
-    defineRule('confirmed', confirmed);
+    defineRule('passwords_mismatch', confirmed);
     defineRule('excluded', excluded);
+    defineRule('country_excluded', excluded);
+
+    configure({
+      // ctx = context
+      generateMessage: (ctx) => {
+        const messages = {
+          required: `The field ${ctx.field} is required.`,
+          min: `The field ${ctx.field} is too short.`,
+          max: `The field ${ctx.field} is too long.`,
+          alpha_spaces: `The field ${ctx.field} may only contain alphabetical characters and spaces.`,
+          email: `The field ${ctx.field} must be a valid email.`,
+          min_value: `The field ${ctx.field} is too low.`,
+          max_value: `The field ${ctx.field} is too high.`,
+          excluded: `You are not allowed to user this value for the field ${ctx.field}`,
+          country_excluded: `Due to the restrctions, we do not accept users form this location.`,
+          passwords_mismatch: `The passwords don't match.`,
+          tos: `You must accept the Terms of Service.`,
+        };
+
+        const message = messages[ctx.rule.name] ? messages[ctx.rule.name] : `The field ${ctx.field} is invalid.`;
+        return message;
+      },
+    });
   },
 };
