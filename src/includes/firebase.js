a3a9957e397eade3ae6bf117b4b44b19e7231d3b
Ancientshield <jacklin2100@gmail.com>
2023-04-15 23:14:57 +0800

    add firebase rules in firebase.js

---
+++ src/includes/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/compat/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyARLgM-ccT1KjnVRRF53osLWBmj0P9V2i8',
+  authDomain: 'music-2b578.firebaseapp.com',
+  projectId: 'music-2b578',
+  storageBucket: 'music-2b578.appspot.com',
+  // messagingSenderId: '888586262714',
+  appId: '1:888586262714:web:7e052c97efc26edb500a83',
+};
+
+export default firebase.initializeApp(firebaseConfig);
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 import VeeValidatePlugin from './includes/validation.js';
+import './includes/firebase.js';
 
 import './assets/base.css';
 import './assets/main.css';
