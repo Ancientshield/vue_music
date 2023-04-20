506f5def2d6e52f3954da37e6a4c5f474f0864ed
Ancientshield <jacklin2100@gmail.com>
2023-04-20 22:35:01 +0800

    use pinia actions

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -104,8 +104,7 @@
 </template>
 
 <script>
-import { auth, usersCollection } from '@/includes/firebase.js';
-import { mapWritableState } from 'pinia';
+import { mapActions } from 'pinia';
 import useUserStore from '@/stores/user.js';
 
 export default {
@@ -131,19 +130,16 @@ export default {
       reg_alert_msg: 'Please wait, your account is being created.',
     };
   },
-  computed: {
-    ...mapWritableState(useUserStore, ['userLoggedIn']),
-  },
   methods: {
+    ...mapActions(useUserStore, { createUser: 'register' }),
     async register(values) {
       this.reg_show_alert = true;
       this.reg_in_submission = true;
       this.reg_alert_variant = 'bg-blue-500';
       this.reg_alert_msg = 'Please wait, your account is being created.';
 
-      let userCredential = null;
       try {
-        userCredential = await auth.createUserWithEmailAndPassword(values.email, values.password);
+        await this.createUser(values);
       } catch (error) {
         this.reg_in_submission = false;
         this.reg_alert_variant = 'bg-red-500';
@@ -151,26 +147,11 @@ export default {
         console.log(error);
         return;
       }
-      try {
-        await usersCollection.add({
-          name: values.name,
-          email: values.email,
-          age: values.age,
-          country: values.country,
-        });
-      } catch (error) {
-        this.reg_in_submission = false;
-        this.reg_alert_variant = 'bg-red-500';
-        this.reg_alert_msg = 'An unexpected error occured. Please try again later.';
-        console.log(error);
-      }
 
       this.reg_alert_variant = 'bg-green-500';
       this.reg_alert_msg = 'success, your accout has been created.';
 
       this.userLoggedIn = true;
-
-      console.log(userCredential);
     },
   },
 };
--- src/stores/user.js
+++ src/stores/user.js
@@ -1,10 +1,26 @@
 import { defineStore } from 'pinia';
+import { auth, usersCollection } from '@/includes/firebase.js';
 
 export default defineStore('user', {
   state: () => {
     return { userLoggedIn: false };
   },
+  // 不一定要用影片中的 code，用上面自己寫的也可。
   // state: () => ({
   //   userLoggedIn: false,
   // }),
+  actions: {
+    async register(values) {
+      await auth.createUserWithEmailAndPassword(values.email, values.password);
+
+      await usersCollection.add({
+        name: values.name,
+        email: values.email,
+        age: values.age,
+        country: values.country,
+      });
+
+      this.userLoggedIn = true;
+    },
+  },
 });
