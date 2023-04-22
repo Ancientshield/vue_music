ada097f41d584d8f41f9de01dfc6a3ed80808a7f
Ancientshield <jacklin2100@gmail.com>
2023-04-22 20:20:18 +0800

    use firebase onAuthStateChanged() to listening if user login at main.js

--- src/main.js
+++ src/main.js
@@ -4,15 +4,21 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 import VeeValidatePlugin from './includes/validation.js';
-import './includes/firebase.js';
+import { auth } from './includes/firebase.js';
 
 import './assets/base.css';
 import './assets/main.css';
 
-const app = createApp(App);
+let app;
 
-app.use(createPinia());
-app.use(router);
-app.use(VeeValidatePlugin);
+auth.onAuthStateChanged(() => {
+  if (!app) {
+    app = createApp(App);
 
-app.mount('#app');
+    app.use(createPinia());
+    app.use(router);
+    app.use(VeeValidatePlugin);
+
+    app.mount('#app');
+  }
+});
