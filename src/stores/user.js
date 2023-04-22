e64885e7f5d0b7cb001af3031e2fb7f8568456c9
Ancientshield <jacklin2100@gmail.com>
2023-04-22 17:36:27 +0800

    connected the user's data in the database to their authentication account.

--- src/stores/user.js
+++ src/stores/user.js
@@ -2,22 +2,29 @@ import { defineStore } from 'pinia';
 import { auth, usersCollection } from '@/includes/firebase.js';
 
 export default defineStore('user', {
-  state: () => {
-    return { userLoggedIn: false };
-  },
-  // 不一定要用影片中的 code，用上面自己寫的也可。
-  // state: () => ({
-  //   userLoggedIn: false,
-  // }),
+  state: () => ({
+    userLoggedIn: false,
+  }),
+  // 不一定要用影片的 code，用下面自己寫的也可。
+  // state: () => {
+  //   return { userLoggedIn: false };
+  // },
   actions: {
     async register(values) {
-      await auth.createUserWithEmailAndPassword(values.email, values.password);
+      const userCredential = await auth.createUserWithEmailAndPassword(
+        values.email,
+        values.password,
+      );
 
-      await usersCollection.add({
+      await usersCollection.doc(userCredential.user.uid).set({
         name: values.name,
         email: values.email,
         age: values.age,
         country: values.country,
+      });
+
+      await userCredential.user.updateProfile({
+        displayName: values.name,
       });
 
       this.userLoggedIn = true;
