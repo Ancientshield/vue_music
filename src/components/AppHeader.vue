213733565c311848567f9ad2395e1aeeeec8671d
Ancientshield <jacklin2100@gmail.com>
2023-04-28 22:05:17 +0800

    finished manage page

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,6 +9,9 @@
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
+          <li>
+            <router-link class="px-2 text-white" to="/about">About</router-link>
+          </li>
           <!-- Navigation Links -->
           <li v-if="!userStore.userLoggedIn">
             <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal">
@@ -17,7 +20,7 @@
           </li>
           <template v-else>
             <li>
-              <a class="px-2 text-white" href="#">Manage</a>
+              <router-link class="px-2 text-white" to="/manage">Manage</router-link>
             </li>
             <li>
               <a class="px-2 text-white" href="#" @click.prevent="userStore.signOut">Logout</a>
--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,9 @@ const routes = [
     path: '/about',
     component: About,
   },
+  {
+    path: '/manage',
+  },
 ];
 
 const router = createRouter({
