afd87ef47067bf65b67b021bef802c989e8cc4cf
Ancientshield <jacklin2100@gmail.com>
2023-04-29 15:04:12 +0800

    add linkExactActiveClass at router index.js

--- src/router/index.js
+++ src/router/index.js
@@ -23,6 +23,7 @@ const router = createRouter({
   // history: createWebHistory(import.meta.env.BASE_URL),
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
+  linkExactActiveClass: 'text-yellow-500',
 });
 
 export default router;
