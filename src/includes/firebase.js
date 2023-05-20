477b8995d353429a699467807387adaeabb0d24d
Ancientshield <jacklin2100@gmail.com>
2023-05-20 19:59:13 +0800

    add the firebase/stoage module, create instance storage object and export this js

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyARLgM-ccT1KjnVRRF53osLWBmj0P9V2i8',
@@ -17,7 +18,8 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
+const storage = firebase.storage();
 
 const usersCollection = db.collection('users');
 
-export { auth, db, usersCollection };
+export { auth, db, usersCollection, storage };
