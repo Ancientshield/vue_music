0e9f699799ecc2b3a095c9c2e4cd9eb85799423d
Ancientshield <jacklin2100@gmail.com>
2023-05-30 22:44:30 +0800

    Handling Errors and Successful Uploads

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -78,10 +78,24 @@ export default {
             icon: 'fas fa-spinner fa-spin',
             text_class: '',
           }) - 1;
-        task.on('state_changed', (snapshot) => {
-          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          this.uploads[uploadIndex].current_progress = progress;
-        });
+        task.on(
+          'state_changed',
+          (snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            this.uploads[uploadIndex].current_progress = progress;
+          },
+          (error) => {
+            this.uploads[uploadIndex].varient = 'bg-red-400';
+            this.uploads[uploadIndex].icon = 'fas fa-times';
+            this.uploads[uploadIndex].text_class = 'text-red-400';
+            console.log(error);
+          },
+          () => {
+            this.uploads[uploadIndex].varient = 'bg-green-400';
+            this.uploads[uploadIndex].icon = 'fas fa-check';
+            this.uploads[uploadIndex].text_class = 'text-green-400';
+          },
+        );
       });
 
       console.log(files);
