cee2beb96a7b07776962c4d182f09daa9380b82b
Ancientshield <jacklin2100@gmail.com>
2023-06-24 21:44:04 +0800

    finished comment time for ascending or decoending sort

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -54,6 +54,7 @@
         </vee-form>
         <!-- Sort Comments -->
         <select
+          v-model="sort"
           class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
         >
           <option value="1">Latest</option>
@@ -66,7 +67,7 @@
   <ul class="container mx-auto">
     <li
       class="p-6 bg-gray-50 border border-gray-200"
-      v-for="comment in comments"
+      v-for="comment in sortedComments"
       :key="comment.docID"
     >
       <!-- Comment Author -->
@@ -97,10 +98,23 @@ export default {
       comment_alert_variant: 'bg-blue-500',
       comment_alert_message: 'Please wait! Your comment is being submitted',
       comments: [],
+      sort: '1',
     };
   },
   computed: {
     ...mapState(useUserStore, ['userLoggedIn']),
+    sortedComments() {
+      // 此時你會發現此寫法 ESLint 會報錯，因為 computed 不能改變 data properties.
+      // return this.comments.sort();
+      // 這時候就要用點小技巧，先用 slice() 創造出一個新的 Array，再 sort()
+      return this.comments.slice().sort((a, b) => {
+        if (this.sort == '1') {
+          return new Date(b.datePosted) - new Date(a.datePosted);
+        }
+
+        return new Date(a.datePosted) - new Date(b.datePosted);
+      });
+    },
   },
   async created() {
     const docSnapshot = await songsCollection.doc(this.$route.params.id).get();
@@ -129,6 +143,8 @@ export default {
       };
 
       await commentsCollection.add(comment);
+
+      this.getComments();
 
       this.comment_in_submission = false;
       this.comment_alert_variant = 'bg-green-500';
