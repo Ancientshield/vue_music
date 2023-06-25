f765d0eb22d4cd6dfaa8bf3c65b8ed17fe3ac6df
Ancientshield <jacklin2100@gmail.com>
2023-06-25 21:31:16 +0800

    use firebase function update to updating the comment count in Song.vue

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -25,7 +25,7 @@
     <div class="bg-white rounded border border-gray-200 relative flex flex-col">
       <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
         <!-- Comment Count -->
-        <span class="card-title">Comments (15)</span>
+        <span class="card-title">Comments {{ song.comment_count }}</span>
         <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
       </div>
       <div class="p-6">
@@ -148,6 +148,11 @@ export default {
       };
 
       await commentsCollection.add(comment);
+
+      this.song.comment_count += 1;
+      await songsCollection.doc(this.$route.params.id).update({
+        comment_count: this.song.comment_count,
+      });
 
       this.getComments();
 
