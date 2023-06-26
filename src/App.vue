da27dede22cbfa26a960579cee8fe0c20a39dba7
Ancientshield <jacklin2100@gmail.com>
2023-06-26 21:09:02 +0800

    separate a child component Player.vue from App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,35 +5,7 @@
   <router-view></router-view>
 
   <!-- Player -->
-  <div class="fixed bottom-0 left-0 bg-white px-4 py-2 w-full">
-    <!-- Track Info -->
-    <div class="text-center">
-      <span class="song-title font-bold">Song Title</span> by
-      <span class="song-artist">Artist</span>
-    </div>
-    <div class="flex flex-nowrap gap-4 items-center">
-      <!-- Play/Pause Button -->
-      <button type="button">
-        <i class="fa fa-play text-gray-500 text-xl"></i>
-      </button>
-      <!-- Current Position -->
-      <div class="player-currenttime">00:00</div>
-      <!-- Scrub Container  -->
-      <div class="w-full h-2 rounded bg-gray-200 relative cursor-pointer">
-        <!-- Player Ball -->
-        <span class="absolute -top-2.5 -ml-2.5 text-gray-800 text-lg" style="left: 50%">
-          <i class="fas fa-circle"></i>
-        </span>
-        <!-- Player Progress Bar-->
-        <span
-          class="block h-2 rounded bg-gradient-to-r from-green-500 to-green-400"
-          style="width: 50%"
-        ></span>
-      </div>
-      <!-- Duration -->
-      <div class="player-duration">03:06</div>
-    </div>
-  </div>
+  <app-player />
 
   <!-- Auth Modal -->
   <auth />
@@ -45,11 +17,12 @@ import Auth from '@/components/Auth.vue';
 import { mapWritableState } from 'pinia';
 import useUserStore from '@/stores/user.js';
 import { auth } from './includes/firebase.js';
+import AppPlayer from './components/Player.vue';
 
 // 舊的寫法在 template 中，要用 html 小寫 <app-header />，不能用 <AppHeader>。
 export default {
   name: 'App',
-  components: { AppHeader, Auth },
+  components: { AppHeader, Auth, AppPlayer },
   computed: {
     ...mapWritableState(useUserStore, ['userLoggedIn']),
   },
