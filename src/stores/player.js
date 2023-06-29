596467104fbae8ba5061b4af465b62fa1e454fc7
Ancientshield <ancientsword520@gmail.com>
2023-06-29 11:26:12 +0800

    finished Duration and Current Position

--- src/stores/player.js
+++ src/stores/player.js
@@ -18,6 +18,12 @@ export default defineStore('player', {
       });
 
       this.sound.play();
+
+      // event listener
+      this.sound.on('play',()=>{
+        // handle the event
+        requestAnimationFrame(this.progress);
+      });
     },
     async toggleAudio() {
       if (!this.sound.playing) {
@@ -30,6 +36,16 @@ export default defineStore('player', {
         this.sound.play();
       }
     },
+    progress(){
+      this.seek = this.sound.seek();
+      this.duration = this.sound.duration();
+
+      // actions 不是 recursive
+      // 所以要不斷回調，就要確認是否正在 playing() 中
+      if(this.sound.playing()){
+        requestAnimationFrame(this.progress);
+      }
+    }
   },
   getters: {
     playing: (state) => {
