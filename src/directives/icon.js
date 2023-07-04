2c9b263ca57e5d739081fe70cb623274ebf86380
Ancientshield <jacklin2100@gmail.com>
2023-07-04 21:53:23 +0800

    use binding.arg to Passing Values to Directives

--- src/directives/icon.js
+++ src/directives/icon.js
@@ -1,5 +1,12 @@
 export default {
-  beforeMount(el) {
-    el.innerHTML += '<i class="fa fa-headphones-alt float-right text-green-400 text-xl"></i>';
+  beforeMount(el, binding) {
+    let iconClass = `fa fa-${binding.value} float-right text-green-400 text-xl`;
+
+    // arg 是 directive 的 property
+    if (binding.arg == 'full') {
+      iconClass = binding.value;
+    }
+    // 綁定的元素有 'full' 字串關鍵字，iconClass 才會改變
+    el.innerHTML += `<i class="${iconClass}"></i>`;
   },
 };
