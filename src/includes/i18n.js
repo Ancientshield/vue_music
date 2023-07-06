e76ef8113027082b29b2be489c64a067ca390a69
Ancientshield <jacklin2100@gmail.com>
2023-07-06 23:33:29 +0800

    set and use i18n JS & JSON files

--- src/includes/i18n.js
+++ src/includes/i18n.js
@@ -1,1 +1,10 @@
 import { createI18n } from 'vue-i18n';
+import en from '../locales/en.json';
+
+export default createI18n({
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages: {
+    en,
+  },
+});
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router';
 import VeeValidatePlugin from './includes/validation.js';
 import { auth } from './includes/firebase.js';
 import Icon from './directives/icon.js';
+import i18n from './includes/i18n.js';
 
 import './assets/base.css';
 import './assets/main.css';
@@ -19,6 +20,7 @@ auth.onAuthStateChanged(() => {
     app.use(createPinia());
     app.use(router);
     app.use(VeeValidatePlugin);
+    app.use(i18n);
     app.directive('icon', Icon);
 
     app.mount('#app');
