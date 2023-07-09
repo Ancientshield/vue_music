c4518b8e10dd954ac9008e1900f74a25f225a161
Ancientshield <jacklin2100@gmail.com>
2023-07-09 14:09:22 +0800

    finished i18n expression and object for difference language

--- src/includes/i18n.js
+++ src/includes/i18n.js
@@ -1,10 +1,12 @@
 import { createI18n } from 'vue-i18n';
 import en from '../locales/en.json';
+import fr from '../locales/fr.json';
 
 export default createI18n({
   locale: 'en',
   fallbackLocale: 'en',
   messages: {
     en,
+    fr,
   },
 });
