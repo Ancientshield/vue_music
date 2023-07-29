409829175e580c80cf0ee8a50afa17441a698bff
Ancientshield <jacklin2100@gmail.com>
2023-07-29 18:31:13 +0800

    finished describe unit test for About.vue

---
+++ src/components/__tests__/about.spec.js
@@ -0,0 +1,8 @@
+import About from '../../views/About.vue';
+import { mount } from '@vue/test-utils';
+
+describe('About.vue', () => {
+  test('renders inner text', () => {
+    const wrapper = mount(About);
+  });
+});
