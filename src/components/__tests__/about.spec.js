f0fa712ace7afa42c589e25334d1d2e451a39e5a
Ancientshield <jacklin2100@gmail.com>
2023-07-29 20:57:59 +0800

    finished about.spec.js Testing the Inner Content

--- src/components/__tests__/about.spec.js
+++ src/components/__tests__/about.spec.js
@@ -1,8 +1,12 @@
 import About from '../../views/About.vue';
-import { mount } from '@vue/test-utils';
+// import { mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 
 describe('About.vue', () => {
   test('renders inner text', () => {
-    const wrapper = mount(About);
+    // const wrapper = mount(About);
+    const wrapper = shallowMount(About);
+
+    expect(wrapper.text()).toContain('about');
   });
 });
