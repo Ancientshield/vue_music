c211af4fa19137e079b31ec942120175f6af761c
Ancientshield <jacklin2100@gmail.com>
2023-07-30 18:55:48 +0800

    finished render song.display_name

--- src/components/__tests__/songitem.spec.js
+++ src/components/__tests__/songitem.spec.js
@@ -16,6 +16,9 @@ describe('SongItem.vue', () => {
         },
       },
     });
-    expect(wrapper.text()).toContain(song.display_name);
+
+    const compositionAuthor = wrapper.find('.text-gray-500');
+
+    expect(compositionAuthor.text()).toBe(song.display_name);
   });
 });
