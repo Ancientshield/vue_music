00893c774266b2a2b430807b10787c7433a95bdc
Ancientshield <jacklin2100@gmail.com>
2023-08-05 21:21:36 +0800

    finished id attritube test

--- src/components/__tests__/songitem.spec.js
+++ src/components/__tests__/songitem.spec.js
@@ -2,7 +2,7 @@ import SongItem from '../SongItem.vue';
 import { shallowMount, RouterLinkStub } from '@vue/test-utils';
 
 describe('SongItem.vue', () => {
-  test('render song.display_name', () => {
+  test('renders song.display_name', () => {
     const song = {
       display_name: 'test',
     };
@@ -20,5 +20,23 @@ describe('SongItem.vue', () => {
     const compositionAuthor = wrapper.find('.text-gray-500');
 
     expect(compositionAuthor.text()).toBe(song.display_name);
+  });
+
+  test('renders song.docID in id attribute', () => {
+    const song = {
+      docID: 'abc',
+    };
+    const wrapper = shallowMount(SongItem, {
+      propsData: {
+        song,
+      },
+      global: {
+        components: {
+          'router-link': RouterLinkStub,
+        },
+      },
+    });
+
+    expect(wrapper.attributes().id).toBe(`song-id-${song.docID}`);
   });
 });
